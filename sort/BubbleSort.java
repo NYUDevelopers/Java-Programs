90e24181bc50b7d649822370bfe96a9771f37b86
Poorni <pr1224@nyu.edu>
2014-11-30 00:37:13 -0500

    Bubble and Insertion sort

---
+++ sort/BubbleSort.java
@@ -0,0 +1,26 @@
+
+public class BubbleSort extends SortBase {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void sort(Object[] a) {
+		calledTimes = 0;
+		runTime = System.currentTimeMillis();
+				
+		if (a == null || a.length <= 1) return;
+		
+		boolean stop = false;
+		
+		while (!stop) {
+			stop = true;
+			for (int i = 0; i < a.length - 1; i++) {
+				calledTimes++;
+				if (((Comparable)a[i]).compareTo(a[i + 1]) > 0) {
+					swap(a, i, i + 1);
+					stop = false;
+				}
+			}
+		}
+		
+		runTime = System.currentTimeMillis() - runTime;
+	}
+}
