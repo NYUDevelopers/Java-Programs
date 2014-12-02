cac203cf23df591fa3c011a4825992c268a942d9
Mayur Kale <myk269@nyu.edu>
2014-12-02 18:39:37 -0500

    Added Selection sort

---
+++ sort/SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort extends SortBase {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void sort(Object[] a) {
+		calledTimes = 0;
+		runTime = System.currentTimeMillis();
+				
+		if (a == null || a.length <= 1) return;
+		
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = i + 1; j < a.length; j++) {
+				calledTimes++;
+				if (((Comparable)a[i]).compareTo(a[j]) > 0) {
+					swap(a, i, j);
+				}
+			}
+		}
+		
+		runTime = System.currentTimeMillis() - runTime;
+	}
+}
